fa2db899f0fcfef9511cadf167e409ed458d29c6
hynjin <hynjin1004@naver.com>
2019-12-03 03:44:21 +0900

    test

--- publishconf.py
+++ publishconf.py
@@ -7,7 +7,8 @@ from __future__ import unicode_literals
 
 import os
 import sys
-sys.path.append(os.curdir+'/tax/')
+sys.path.append(os.curdir)
+sys.path.append(/tax/)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
